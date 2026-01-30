60136be89d7712b2f4dc4156fe1a96f5e886804d
STARQ <tariqueahmed1904@gmail.com>
2026-01-31 04:53:53 +0530

    chore: add backend setup, postgres configuration, and project documentation

---
+++ backend/src/main/java/com/starq/operations/operations/tracker/OperationsRequestTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.starq.operations.operations.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsRequestTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsRequestTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/starq/operations/operations/tracker/OperationsRequestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starq.operations.operations.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsRequestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
